f0bd5cedcab783c491b5c2e6978969d8917eab40
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2023-11-13 13:15:27 -0300

    Use bytes instead of ints, set program counter

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def load_rom(path, mem):
     try:
         with open(path, "rb") as f:
             rom = bytearray(f.read())           # Read file bytes into variable
-            first_byte = 512                    # Establish beginning of ROM in memory space (0x200 here)
-            last_byte = 512 + len(rom)          # Calculate end of bytestream 
+            first_byte = 0x200                    # Establish beginning of ROM in memory space (0x200 by default in CHIP-8)
+            last_byte = 0x200 + len(rom)          # Calculate end of bytestream 
             mem[first_byte:last_byte] = rom     # Replace bytes in memory with ROM file
 
         return True                 # Return True if success
@@ -22,19 +22,15 @@ def main():
     
     # Let's also get the registers out of the way
     vx_registers = bytearray(16)
-    stack_pointer = bytearray(1)
-    i_register = 0
-    pcounter = 0
+    stack_pointer = bytes(1)
+    i_register = bytes(2)
+    pcounter = bytes(2)
 
     # Initializing the stack
-    stack = [0]*16
-
-    print(len(main_memory))
+    stack = [bytes(2)]*16
 
     # Try to load ROM into main memory at 0x200
     load_rom(sys.argv[1], main_memory)
-
-    print(len(main_memory))
 
     # Fire up our display
     screen = display.initialize()
@@ -42,6 +38,11 @@ def main():
     # Set up our clock
     clock = pygame.time.Clock()
     clock.tick(10) #TODO: check ideal framerate
+
+    # Set the program counter to the default entry point
+    pcounter = bytes(main_memory[0x200:0x202]) #TODO: why do i need to use 0x202 here to read the first two bytes? theoretically this would return 3 bytes but it doesn't...
+
+    print(pcounter)
 
     # The interpreter is now running!
     # This variable keeps track if we're supposed to stop.
