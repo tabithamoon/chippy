1579d2d15f7bacf89dface56e317082b47be9517
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2023-11-13 14:54:52 -0300

    Switched to int arrays, implemented more opcodes

--- main.py
+++ main.py
@@ -1,36 +1,25 @@
 from src import display
+from src import file
 import pygame
 import sys
 
 running = False
 
-def load_rom(path, mem):
-    try:
-        with open(path, "rb") as f:
-            rom = bytearray(f.read())           # Read file bytes into variable
-            first_byte = 0x200                    # Establish beginning of ROM in memory space (0x200 by default in CHIP-8)
-            last_byte = 0x200 + len(rom)          # Calculate end of bytestream 
-            mem[first_byte:last_byte] = rom     # Replace bytes in memory with ROM file
-
-        return True                 # Return True if success
-    except IOError:
-        return False                # Return False if we ran into a problem
-
 def main():
     # Feel like the first thing to do is initialize memory, so let's do that
-    main_memory = bytearray(4096)
+    main_memory = [0]*4096
     
     # Let's also get the registers out of the way
-    vx_registers = bytearray(16)
-    stack_pointer = bytes(1)
-    i_register = bytes(2)
-    pcounter = bytes(2)
+    vx_registers = [0]*16
+    stack_pointer = 0
+    i_register = 0
+    pcounter = 0
 
     # Initializing the stack
-    stack = [bytes(2)]*16
+    stack = [0]*16
 
     # Try to load ROM into main memory at 0x200
-    load_rom(sys.argv[1], main_memory)
+    file.load_rom(sys.argv[1], main_memory)
 
     # Fire up our display
     screen = display.initialize()
@@ -40,9 +29,7 @@ def main():
     clock.tick(10) #TODO: check ideal framerate
 
     # Set the program counter to the default entry point
-    pcounter = bytes(main_memory[0x200:0x202]) #TODO: why do i need to use 0x202 here to read the first two bytes? theoretically this would return 3 bytes but it doesn't...
-
-    print(pcounter)
+    pcounter = 0x200
 
     # The interpreter is now running!
     # This variable keeps track if we're supposed to stop.
@@ -50,13 +37,53 @@ def main():
 
     # The main logic loop
     while running:
-        # Allow the user to quit by pressing the close button
-        # Kind of important
+
+        # Main interpreter logic goes here!
+        # Get current instruction at program counter
+        curr_instr = (main_memory[pcounter] << 8) + main_memory[pcounter + 1]
+        print(curr_instr)
+
+        match curr_instr:
+            # CLS - Clear the display
+            case 0x00E0:
+                display.cls(screen)
+                pcounter += 2
+            
+            # RET - Return from a subroutine
+            case 0x00EE:
+                pcounter = stack[stack_pointer]
+                stack_pointer -= 1
+            
+            # JP addr - Jump to location 0x1nnn
+            case _ if curr_instr & 0xF000 == 0x1000:
+                pcounter = curr_instr & 0x0FFF
+
+            # CALL addr - Call subroutine at 0x2nnn
+            case _ if curr_instr & 0xF000 == 0x2000:
+                stack[stack_pointer] = pcounter
+                stack_pointer += 1
+                pcounter = curr_instr & 0x0FFF
+
+            # SE Vx, byte - (0x3xkk) Skip next instruction if Vx = kk
+            case _ if curr_instr & 0xF000 == 0x3000:
+                reg_tocheck = (curr_instr & 0x0F00) >> 8
+                val_tocheck = curr_instr & 0x00FF
+                if (vx_registers[reg_tocheck] == val_tocheck):
+                    pcounter += 4
+                else:
+                    pcounter += 2
+
+            # LD Vx, byte - (0x6xkk) Set Vx = kk
+            case _ if curr_instr & 0xF000 == 0x6000:
+                mod_register = (curr_instr & 0x0F00) >> 8
+                vx_registers[mod_register] = (curr_instr & 0x00FF)
+                pcounter += 2
+
+        # Allow the user to quit by pressing the close button, kind of important
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        
-        # Main interpreter logic goes here!
+
 
         # Flip and display at end of execution loop
         display.flip(screen)
