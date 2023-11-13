a5d95c71d4da6342d6d11f6fb88d71cd5cfcba6e
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2023-11-13 12:30:13 -0300

    Multiple changes Added memory initialization, basic display functions, ROM loading & more

--- main.py
+++ main.py
@@ -1,5 +1,64 @@
+from src import display
+import pygame
+import sys
+
+running = False
+
+def load_rom(path, mem):
+    try:
+        with open(path, "rb") as f:
+            rom = bytearray(f.read())           # Read file bytes into variable
+            first_byte = 512                    # Establish beginning of ROM in memory space (0x200 here)
+            last_byte = 512 + len(rom)          # Calculate end of bytestream 
+            mem[first_byte:last_byte] = rom     # Replace bytes in memory with ROM file
+
+        return True                 # Return True if success
+    except IOError:
+        return False                # Return False if we ran into a problem
+
 def main():
-    print("The first beginnings...")
+    # Feel like the first thing to do is initialize memory, so let's do that
+    main_memory = bytearray(4096)
+    
+    # Let's also get the registers out of the way
+    vx_registers = bytearray(16)
+    stack_pointer = bytearray(1)
+    i_register = 0
+    pcounter = 0
+
+    # Initializing the stack
+    stack = [0]*16
+
+    print(len(main_memory))
+
+    # Try to load ROM into main memory at 0x200
+    load_rom(sys.argv[1], main_memory)
+
+    print(len(main_memory))
+
+    # Fire up our display
+    screen = display.initialize()
+
+    # Set up our clock
+    clock = pygame.time.Clock()
+    clock.tick(10) #TODO: check ideal framerate
+
+    # The interpreter is now running!
+    # This variable keeps track if we're supposed to stop.
+    running = True
+
+    # The main logic loop
+    while running:
+        # Allow the user to quit by pressing the close button
+        # Kind of important
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        # Main interpreter logic goes here!
+
+        # Flip and display at end of execution loop
+        display.flip(screen)
 
 if __name__ == "__main__":
     main()
---
+++ src/display.py
@@ -0,0 +1,18 @@
+import pygame
+from pygame.locals import *
+
+color_wht = pygame.Color(255, 255, 255, 255)
+color_blk = pygame.Color(0, 0, 0, 255)
+
+def initialize():
+    pygame.init()
+    screen = pygame.display.set_mode((640,320))
+    return screen
+
+def flip(screen):
+    pygame.display.flip()
+    screen.fill(color_blk)
+
+def draw_pixel(screen, x, y):
+    pixel = pygame.Rect(x * 10, y * 10, 10, 10)
+    pygame.draw.rect(screen, color_wht, pixel)
